5b6faf420b0b2bcc56c886d2545f863460762fce
Maksimtikhon <tikhon.maxim@gmail.com>
2016-07-27 21:34:29 +0300

    Added xsd

---
+++ EPAM.Mentoring.AdvancedXml/Task1. Xsd/XmlVerificationTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Schema;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Task1.Xsd
+{
+	[TestClass]
+	public class XmlVerificationTest
+	{
+		XmlReaderSettings _settings;
+		private int _errorCount;
+
+		[TestInitialize]
+		public void Init()
+		{
+			_settings = new XmlReaderSettings();
+
+			_settings.Schemas.Add("http://library.by/catalog", "../../books.xsd");
+			_settings.ValidationEventHandler +=
+				delegate (object sender, ValidationEventArgs e)
+				{
+					_errorCount++;
+					Console.WriteLine("[{0}:{1}] {2}", e.Exception.LineNumber, e.Exception.LinePosition, e.Message);
+				};
+
+			_settings.ValidationFlags = _settings.ValidationFlags | XmlSchemaValidationFlags.ReportValidationWarnings;
+			_settings.ValidationType = ValidationType.Schema;
+		}
+
+		[TestMethod]
+		public void CheckValidXml()
+		{
+			_errorCount = 0;
+			XmlReader reader = XmlReader.Create("../../books.xml", _settings);
+
+			while (reader.Read()) { }
+
+			Assert.AreEqual(_errorCount, 0);
+		}
+
+		[TestMethod]
+		public void CheckInvalidXml()
+		{
+			_errorCount = 0;
+			XmlReader reader = XmlReader.Create("../../booksInvalid.xml", _settings);
+
+			while (reader.Read()) { }
+
+			Assert.AreNotEqual(_errorCount, 0);
+		}
+	}
+}
